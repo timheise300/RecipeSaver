f47829f9bf3d84c9d3bbec95df182c1a3126a8ca
Tim Heise <timheise300@gmail.com>
2015-03-17 13:45:37 +0000

    Committing user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'home' => 'static_pages#home'
+  get 'signup' => 'users#new'
 
-  get 'help' => 'static_pages#help'
+  get 'home'   => 'static_pages#home'
+
+  get 'help'   => 'static_pages#help'
   
-  get 'about' => 'static_pages#about'
+  get 'about'  => 'static_pages#about'
   
-  root 'static_pages#home'
+  root            'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
