4517fbf3de591f0b9c715232fc785ae8b6fffdce
Tim Heise <timheise300@gmail.com>
2015-03-17 13:11:32 +0000

    Committing layout and bootstrap

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 
+gem 'bootstrap-sass', '3.2.0.0'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
   
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
   
   root 'static_pages#home'
 
