e667eb259a4f266a426291bc4605d065975ec9a1
Tim Heise <timheise300@gmail.com>
2015-05-06 14:14:28 +0000

    Committing changes to carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAITRGZTDPSK4CZAFQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory = ENV['recipesaverimages']
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
