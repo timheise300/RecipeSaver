fd94a76d87e4db4b32f214aeb7f7cabe707ac2c5
Tim Heise <timheise300@gmail.com>
2015-05-14 13:42:52 +0000

    Committing changes for testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
       else
-        message = "Account not activated. "
+        message  = "Account not activated. "
         message += "Check your email for the activation link."
         flash[:warning] = message
         redirect_to root_url
@@ -21,6 +21,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
   
   def destroy
     log_out if logged_in?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @user = users(:michael)
   end
-  
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -15,7 +15,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-  
+
   test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
@@ -36,12 +36,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
-  
+
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_not_nil cookies['remember_token']
   end
-  
+
   test "login without remembering" do
     log_in_as(@user, remember_me: '0')
     assert_nil cookies['remember_token']
